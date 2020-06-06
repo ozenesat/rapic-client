31098d49bf267957d9310bd721758e8170c59dfa
Eren Algan <eren@uber.com>
2020-06-06 15:31:21 -0700

    Another try on static generation

--- src/rapicio/next.config.js
+++ src/rapicio/next.config.js
@@ -28,6 +28,14 @@ module.exports = withPlugins(
         "/": { page: "/" },
         "/login": { page: "/login" }
       }
-    }
+    },
+    generateBuildId: async () => {
+      if (process.env.BUILD_ID) {
+        return process.env.BUILD_ID;
+      } else {
+        return `${new Date().getTime()}`;
+      }
+    },
+    exportTrailingSlash: true,
   }
 );
