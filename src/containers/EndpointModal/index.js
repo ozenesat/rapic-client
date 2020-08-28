567d209275036b8010f3d91b6da4d9735380963b
ozenesat <ozenesat@gmail.com>
2020-08-28 13:15:16 -0400

    pipeline error

--- src/containers/EndpointModal/index.js
+++ src/containers/EndpointModal/index.js
@@ -2,20 +2,15 @@ import { useState } from "react";
 import Modal from "react-modal";
 import { useRouter } from "next/router";
 import Heading from "common/src/components/Heading";
-import { Icon } from "react-icons-kit";
-import { iosTrash } from "react-icons-kit/ionicons/iosTrash";
 
 import {
   Container,
   Content,
-  FieldsWrapper,
-  Field,
   ButtonWrapper,
 } from "pagestyles/projects/endpoints/add/add.style";
 import { Title, Section } from "containers/ProjectModal/projectmodal.style";
 import Input from "common/src/components/Input";
 import Button from "common/src/components/Button";
-import DropdownMenu from "common/src/components/Dropdown";
 import { Loading } from "components/Loading";
 import MessageBox from "containers/MessageBox";
 import { ModalStyles } from "containers/ProjectModal/projectmodal.style";
@@ -32,25 +27,6 @@ function EndpointAddModal({ isModalOpen, closeModal, project }) {
   const [message, setMessage] = useState({ text: "", type: "" });
   const { username } = getSessionCookie(null);
   const setGlobalState = useActionState();
-  //   function handleAddField() {
-  //     fields.push({ name: "", fieldtype: "" });
-  //     addField([].concat(fields));
-  //   }
-
-  //   function deleteField(index) {
-  //     fields.splice(index, 1);
-  //     addField([].concat(fields));
-  //   }
-
-  //   function changeType(index, fieldtype) {
-  //     fields[index].fieldtype = fieldtype;
-  //     addField([].concat(fields));
-  //   }
-
-  //   function handleOnChangeField(index, name) {
-  //     fields[index].name = name;
-  //     addField([].concat(fields));
-  //   }
 
   function resetModalState() {
     onChangeDescription("");
@@ -59,7 +35,7 @@ function EndpointAddModal({ isModalOpen, closeModal, project }) {
     closeModal();
   }
 
-  function createEnpoint() {
+  function createEndPoint() {
     setLoading(true);
     let enpoint = {
       app: id * 1,
@@ -83,26 +59,6 @@ function EndpointAddModal({ isModalOpen, closeModal, project }) {
           type: "error",
         });
       });
-  }
-
-  function checkFields() {
-    // if (fields.length < 1) {
-    //   setMessage({
-    //     text: "There should be at least one field.",
-    //     type: "error",
-    //   });
-    //   return;
-    // }
-    // for (var field of fields) {
-    //   if (field.fieldtype == "" || field.name == "") {
-    //     setMessage({
-    //       text: "Please fill the all fields.",
-    //       type: "error",
-    //     });
-    //     return;
-    //   }
-    // }
-    createEnpoint();
   }
 
   return (
@@ -139,40 +95,8 @@ function EndpointAddModal({ isModalOpen, closeModal, project }) {
               className="endpoint-description"
             />
           </Section>
-          {/* <Button
-            title="+ Add Fields"
-            id="add-fields"
-            onClick={() => handleAddField()}
-          />
-          <FieldsWrapper>
-            {fields &&
-              fields.map((item, index) => (
-                <Field>
-                  <DropdownMenu
-                    content={item.fieldtype !== "" ? item.fieldtype : "TYPE >"}
-                    dropdownItems={["boolean", "float", "integer", "text"]}
-                    className={`field-type dropdown-${index}`}
-                    onSelect={(fieldtype) => changeType(index, fieldtype)}
-                  />
-                  <Input
-                    required
-                    inputType="text"
-                    placeholder="Example: id"
-                    name="field-name"
-                    value={item.name}
-                    onChange={(value) => handleOnChangeField(index, value)}
-                    className={`field-name input-${index}`}
-                  />
-                  <Icon
-                    icon={iosTrash}
-                    className={`delete button-${index}`}
-                    onClick={() => deleteField(index)}
-                  />
-                </Field>
-              ))}
-          </FieldsWrapper> */}
           <ButtonWrapper>
-            <Button title="Create Endpoint" id="create" onClick={checkFields} />
+            <Button title="Create Endpoint" id="create" onClick={createEndpoint} />
             <Button title="Cancel" id="cancel" onClick={closeModal} />
           </ButtonWrapper>
           <MessageBox message={message.text} type={message.type} />
